3d73c61c2fd17bed5937d4912432a5e8b0145841
Grisha <grigorije05@gmail.com>
2022-12-19 14:45:59 +0100

    play and stop button

--- src/components/genre-card.ts
+++ src/components/genre-card.ts
@@ -3,11 +3,11 @@ export const createCards = (genres: any, parent: HTMLElement) => {
         const main = document.createElement('div')
         const title = document.createElement('p')
         const now_playing = document.createElement('p') //to fetch
-
+        const playstop_img = document.createElement('img')
         const dummy_1 = document.createElement('div')
         const dummy_2 = document.createElement('div')
         
-        main.className = `border-4 rounded-xl inline-block m-3 overflow-hidden w-[15em] h-[8em] shadow-xl relative`
+        main.className = `border-4 rounded-xl inline-block m-3 overflow-hidden w-[15em] h-[8em] shadow-xl relative cursor-pointer hover:shadow-2xl`
         main.style.borderColor = data['color'];
         main.style.backgroundColor = data['color'];
 
@@ -22,14 +22,20 @@ export const createCards = (genres: any, parent: HTMLElement) => {
 
         dummy_2.className = "brightness-75 absolute w-[15em] h-[8em] top-[20px] left-[150px] rotate-[100deg]"
         dummy_2.style.backgroundColor = data['color'];
+
+        playstop_img.src = "/play.svg"
+        playstop_img.className = "absolute bottom-2 left-2 h-[2.5em]"
         
         
         main.appendChild(title);
         main.appendChild(now_playing);
         main.appendChild(dummy_1);
         main.appendChild(dummy_2);
+        main.appendChild(playstop_img);
         
 
         parent.appendChild(main);
     }
 }
+
+// icons source https://www.svgrepo.com/collection/music-control-panel/

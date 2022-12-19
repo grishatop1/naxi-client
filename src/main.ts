74b0eeb43b4a6c33f4b36df7dc6181777b36b3cf
Grisha <grigorije05@gmail.com>
2022-12-19 15:08:49 +0100

    make more responsive

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,10 @@ let createCategorySection = (category: string) => {
     let element = document.createElement('section')
     let title = document.createElement('h1')
     let content = document.createElement('div')
+    element.className = 'm-20 tablet:m-10 tablet:my-20'
     title.innerHTML = category
-    title.className = 'text-4xl m-3'
-    element.className = 'm-20'
+    title.className = 'text-4xl m-3 tablet:m-5 tablet:text-center'
+    content.className = 'flex flex-wrap tablet:justify-center'
     element.appendChild(title)
     element.appendChild(content)
     document.getElementById('content').appendChild(element)
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'phone': {'max': '390px'},
+      'tablet': {'max': '640px'},
+      'tablet-xl': {'max': '835px'},
+      'laptop': {'max': '1024px'},
+      'desktop': {'max': '1280px'},
+    },
   },
   plugins: [],
 }

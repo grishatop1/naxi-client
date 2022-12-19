b74234e3c5769340bc97f78350f80a6c49d50552
Grisha <grigorije05@gmail.com>
2022-12-19 14:37:24 +0100

    little decoration thing

--- src/components/genre-card.ts
+++ src/components/genre-card.ts
@@ -3,8 +3,11 @@ export const createCards = (genres: any, parent: HTMLElement) => {
         const main = document.createElement('div')
         const title = document.createElement('p')
         const now_playing = document.createElement('p') //to fetch
+
+        const dummy_1 = document.createElement('div')
+        const dummy_2 = document.createElement('div')
         
-        main.className = `border-4 rounded-xl inline-block m-3 overflow-hidden w-[15em] h-[8em] shadow-xl`
+        main.className = `border-4 rounded-xl inline-block m-3 overflow-hidden w-[15em] h-[8em] shadow-xl relative`
         main.style.borderColor = data['color'];
         main.style.backgroundColor = data['color'];
 
@@ -12,10 +15,21 @@ export const createCards = (genres: any, parent: HTMLElement) => {
         title.className = "font-bold text-white mt-2 ml-2";
 
         now_playing.innerHTML = "Playing: John Doe - Relax"
-        now_playing.className = "text-gray-300 ml-2"
+        now_playing.className = "text-gray-300 ml-2 z-[1000]"
 
+        dummy_1.className = "brightness-75 absolute w-[15em] h-[8em] rotate-12 top-[100px] left-[150px]"
+        dummy_1.style.backgroundColor = data['color'];
+
+        dummy_2.className = "brightness-75 absolute w-[15em] h-[8em] top-[20px] left-[150px] rotate-[100deg]"
+        dummy_2.style.backgroundColor = data['color'];
+        
+        
         main.appendChild(title);
         main.appendChild(now_playing);
+        main.appendChild(dummy_1);
+        main.appendChild(dummy_2);
+        
+
         parent.appendChild(main);
     }
 }

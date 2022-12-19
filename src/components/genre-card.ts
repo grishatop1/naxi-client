5c0e6d8c22b95f9c167a04b12fde135fc4e8ac24
Grisha <grigorije05@gmail.com>
2022-12-19 20:39:02 +0100

    little transition

--- src/components/genre-card.ts
+++ src/components/genre-card.ts
@@ -24,7 +24,7 @@ export const createCards = (genres: any, parent: HTMLElement) => {
         dummy_2.style.backgroundColor = data['color'];
 
         playstop_img.src = "/play.svg"
-        playstop_img.className = "absolute bottom-2 left-2 h-[2.5em] group-hover:scale-[1.10]"
+        playstop_img.className = "absolute bottom-2 left-2 h-[2.5em] group-hover:scale-[1.10] transition-all"
         
         
         main.appendChild(title);

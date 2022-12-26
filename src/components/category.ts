1fe03123512fb248cc41a4579bbc979261eaf2e1
Stefan Šućur <stefansucur0@gmail.com>
2022-12-26 14:21:17 +0100

    Fixing things
    Centred the title text within the title bar.
    Took the theme button back out of the title bar.
    Made the category text, and all the info panel text apart from metadata, not selectable.

--- src/components/category.ts
+++ src/components/category.ts
@@ -4,7 +4,7 @@ export let createCategoryElement = (category: string) => {
     let content = document.createElement('div')
     element.className = 'mt-10 mb-10 ml-10 tablet-xl:m-10 tablet-xl:my-20'
     title.innerHTML = category
-    title.className = 'text-4xl m-3 tablet-xl:m-5 tablet-xl:text-center dark:text-white'
+    title.className = 'text-4xl m-3 tablet-xl:m-5 tablet-xl:text-center dark:text-white select-none'
     content.className = 'flex flex-wrap tablet-xl:justify-center items-center card-content'
     element.appendChild(title)
     element.appendChild(content)
